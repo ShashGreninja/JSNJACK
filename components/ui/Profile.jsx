dbe55a6a255f8b78eb67694c19125bf5c31f3678
soumyajyoti mohanta <soumyajyotimohanta@gmail.com>
2025-01-19 21:11:29 +0530

    example commit for njack

---
+++ components/ui/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Profile = () => {
+    return (
+        <>
+            <div>
+                <button onClick={()=>{alert("hello")}}> Press </button>
+            </div>
+        </>
+    );
+};
+
+export default Profile;
